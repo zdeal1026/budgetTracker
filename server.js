96caf0b108ffe0fad4e6f64573e17277fd31adc5
Zdeal <jedimastershake@gmail.com>
2021-09-02 21:48:55 -0400

    updated server/ trouble shooting

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
+  
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 const app = express();
 
@@ -15,9 +16,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+//connects to mongoose
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 // routes
