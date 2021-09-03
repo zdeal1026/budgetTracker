6ec610881e9535de3424d84db69322801511bd2d
Zdeal <jedimastershake@gmail.com>
2021-09-02 23:02:19 -0400

    working model

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,44 +1,59 @@
+//files to cache
 const FILES_TO_CACHE = [
   "/",
-  "/index.html",
-  "/manifest.json",
-  "/db.js",
-  "/index.js",
-  "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
-  ];
-  
-  const PRECACHE = 'applicationcache-v1';
-  const RUNTIME = 'runtime';
-  
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        //.then(self.skipWaiting())
-    );
-  });
-  
-  
-  self.addEventListener('fetch', (event) => {
-    if (event.request.url.startsWith("/api/")) {
-      event.respondWith(
-        caches.match(event.request).then((cachedResponse) => {
-          if (cachedResponse) {
-            return cachedResponse;
+  "/index.html",
+  "/index.js",
+  "/styles.css",
+  "/db.js",
+  "/manifest.webmanifest",
+];
+
+// install
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing cache data", key);
+            return caches.delete(key);
           }
-  
-          return caches.open(RUNTIME).then((cache) => {
-            return fetch(event.request).then((response) => {
-              return cache.put(event.request, response.clone()).then(() => {
-                return response;
-              });
-            });
-          });
         })
       );
-    }
-  });
-  
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
+        });
+      })
+    );
+  }
+});
