0a407c60533cd35ab0ea318d7b03f06e18cb4c2c
Zdeal <jedimastershake@gmail.com>
2021-09-02 21:35:49 -0400

    trouble shooting

--- public/db.js
+++ public/db.js
@@ -4,22 +4,17 @@ let db;
 const request = indexedDB.open('BudgetDB', 1);
 
 request.onupgradeneeded = function (e) {
-  console.log('Upgrade needed in IndexDB');
-
-  const { oldVersion } = e;
-  const newVersion = e.newVersion || db.version;
-
-  console.log(`DB Updated from version ${oldVersion} to ${newVersion}`);
-
   db = e.target.result;
 
-  if (db.objectStoreNames.length === 0) {
-    db.createObjectStore('BudgetStore', { autoIncrement: true });
-  }
+  db.createObjectStore("BudgetStore", { autoIncrement: true });
 };
 
 request.onerror = function (e) {
-  console.log(`Woops! ${e.target.errorCode}`);
+  console.log(`Error`);
+};
+
+request.onerror = function (e) {
+  console.log(`Error ${e.target.errorCode}`);
 };
 
 function checkDatabase() {
